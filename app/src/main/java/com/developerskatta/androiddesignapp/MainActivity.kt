d5bdac74646a1643443aeaae100b238eea1557a7
Keshav Katad <kkatad123@gmail.com>
2020-08-10 09:06:23 +0530

    add app name variable

--- app/src/main/java/com/developerskatta/androiddesignapp/MainActivity.kt
+++ app/src/main/java/com/developerskatta/androiddesignapp/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.developerskatta.androiddesignapp
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    var appName = "Android design app"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
