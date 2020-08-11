2441f10420ec0eacd463f8484368c56e88c10317
Keshav Katad <kkatad123@gmail.com>
2020-08-11 09:17:43 +0530

    create login screen with linear layout properties

---
+++ app/src/main/java/com/developerskatta/androiddesignapp/LinearLayoutGravityActivity.kt
@@ -0,0 +1,11 @@
+package com.developerskatta.androiddesignapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LinearLayoutGravityActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_linear_layout_gravity)
+    }
+}
