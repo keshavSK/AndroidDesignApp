3afe9bff0b9136e27d7288602097ca165e1fa6cf
Keshav Katad <kkatad123@gmail.com>
2020-08-12 09:24:38 +0530

    working with relative layout and create login screen

---
+++ app/src/main/java/com/developerskatta/androiddesignapp/LoginScreenWithRelativeLayoutActivity.kt
@@ -0,0 +1,11 @@
+package com.developerskatta.androiddesignapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginScreenWithRelativeLayoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_screen_with_relative_layout)
+    }
+}
